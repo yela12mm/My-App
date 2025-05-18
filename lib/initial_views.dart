c56df598b173f0a7d74371313c94dcba0e658455
yela12mm <yelainesmartinez9@gmail.com>
2025-05-17 22:23:54 -0500

    segundo commit

--- lib/initial_views.dart
+++ lib/initial_views.dart
@@ -1,13 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold());
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 236, 202, 224),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Align(
+              alignment: Alignment.center,
+              child: Text(
+                'WELCOME',
+                style: GoogleFonts.chewy(
+                  textStyle: TextStyle(
+                    fontSize: 50,
+                    color: const Color.fromARGB(255, 241, 238, 212),
+                    shadows: [
+                      Shadow(
+                        offset: Offset(2.0, 2.0),
+                        blurRadius: 4.0,
+                        color: Colors.black26,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.center,
+              child: Padding(
+                padding: EdgeInsets.all(20.0),
+                child: Text(
+                  'Activa tu potencial: organiza tu día, recuerda tus tareas y alcanza tus metas.',
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.nunito(
+                    textStyle: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: const Color.fromARGB(255, 248, 243, 224),
+                      shadows: [
+                        Shadow(
+                          offset: Offset(2.0, 2.0),
+                          blurRadius: 4.0,
+                          color: Colors.black26,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Image.network(
+              "https://i.imgur.com/z5yImr1.png ",
+              width: 200,
+              height: 200,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,9 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 95, 183, 103),
+        ),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
