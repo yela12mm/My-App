229a0411e6aa21071ba3f7f78d5dc14bad73144b
yela12mm <yelainesmartinez9@gmail.com>
2025-05-21 19:58:48 -0500

    commit once

--- lib/feature/home/presentation/views/initial_views.dart
+++ lib/feature/home/presentation/views/initial_views.dart
@@ -1,47 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:timebox/feature/home/presentation/bloc/home_bloc.dart';
 
-Scaffold initial() {
-  return Scaffold(
-    backgroundColor: Color.fromARGB(255, 236, 202, 224),
-    body: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Align(
-          alignment: Alignment.center,
-          child: Text(
-            'WELCOME',
-            style: GoogleFonts.chewy(
-              textStyle: TextStyle(
-                fontSize: 50,
-                color: Color.fromARGB(255, 241, 238, 212),
-                shadows: [
-                  Shadow(
-                    offset: Offset(2.0, 2.0),
-                    blurRadius: 4.0,
-                    color: Colors.black26,
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
-        Align(
-          alignment: Alignment.center,
-          child: Padding(
-            padding: EdgeInsets.all(20.0),
+class Initial extends StatelessWidget {
+  const Initial({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 236, 202, 224),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Align(
+            alignment: Alignment.center,
             child: Text(
-              'Activa tu potencial: organiza tu día, recuerda tus tareas y alcanza tus metas.',
-              textAlign: TextAlign.center,
-              style: GoogleFonts.nunito(
+              'WELCOME',
+              style: GoogleFonts.chewy(
                 textStyle: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Color.fromARGB(255, 248, 243, 224),
+                  fontSize: 50,
+                  color: Color.fromARGB(255, 241, 238, 212),
                   shadows: [
                     Shadow(
-                      offset: Offset(1.0, 1.0),
-                      blurRadius: 1.0,
+                      offset: Offset(2.0, 2.0),
+                      blurRadius: 4.0,
                       color: Colors.black26,
                     ),
                   ],
@@ -49,39 +32,64 @@ Scaffold initial() {
               ),
             ),
           ),
-        ),
-        Image.network(
-          "https://i.imgur.com/z5yImr1.png ",
-          width: 225,
-          height: 225,
-        ),
-        SizedBox(height: 100),
-        ElevatedButton(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: Color.fromARGB(255, 236, 232, 202),
-            side: const BorderSide(color: Colors.black, width: 0.5),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(30),
-            ),
-            elevation: 20,
-            shadowColor: Colors.black38,
-            padding: EdgeInsets.symmetric(horizontal: 100, vertical: 20),
-          ),
-          onPressed: () {},
-          final homeBloc = BlocProvider.of<HomeBloc>(context);
-          homeBloc.add(HomeSearchPressed());
-          child: Text(
-            'Star Now',
-            style: GoogleFonts.nunito(
-              textStyle: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-                color: Color.fromARGB(255, 175, 116, 174),
+          Align(
+            alignment: Alignment.center,
+            child: Padding(
+              padding: EdgeInsets.all(20.0),
+              child: Text(
+                'Activa tu potencial: organiza tu día, recuerda tus tareas y alcanza tus metas.',
+                textAlign: TextAlign.center,
+                style: GoogleFonts.nunito(
+                  textStyle: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromARGB(255, 248, 243, 224),
+                    shadows: [
+                      Shadow(
+                        offset: Offset(1.0, 1.0),
+                        blurRadius: 1.0,
+                        color: Colors.black26,
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ),
           ),
-        ),
-      ],
-    ),
-  );
+          Image.network(
+            "https://i.imgur.com/z5yImr1.png",
+            width: 225,
+            height: 225,
+          ),
+          SizedBox(height: 100),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Color.fromARGB(255, 236, 232, 202),
+              side: const BorderSide(color: Colors.black, width: 0.5),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30),
+              ),
+              elevation: 20,
+              shadowColor: Colors.black38,
+              padding: EdgeInsets.symmetric(horizontal: 100, vertical: 20),
+            ),
+            onPressed: () {
+              final homeBloc = BlocProvider.of<HomeBloc>(context);
+              homeBloc.add(HomeSearchPressed());
+            },
+            child: Text(
+              'Start Now',
+              style: GoogleFonts.nunito(
+                textStyle: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                  color: Color.fromARGB(255, 175, 116, 174),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,33 @@
-<<<<<<< HEAD
-
-=======
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:timebox/feature/home/presentation/views/fealure.dart';
+import 'package:timebox/feature/home/presentation/bloc/home_bloc.dart';
+import 'package:timebox/feature/home/presentation/views/failure_views.dart';
 import 'package:timebox/feature/home/presentation/views/initial_views.dart';
-import 'package:timebox/feature/home/presentation/views/loading.dart';
-import 'package:timebox/feature/home/presentation/views/success.dart';
+import 'package:timebox/feature/home/presentation/views/loading_views.dart';
+import 'package:timebox/feature/home/presentation/views/success_views.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Builder(
-      builder: (context) {
-        return MaterialApp(home: Scaffold());
-      },
+    return BlocProvider(
+      create: (context) => HomeBloc(),
+      child: MaterialApp(
+        title: 'My APP',
+        home: BlocBuilder<HomeBloc, HomeState>(
+          builder: (context, state) {
+            if (state is HomeLoadSuccess) {
+              return Success();
+            } else if (state is HomeLoadInProgress) {
+              return loading();
+            } else if (state is HomeLoadFailure) {
+              return failure();
+            }
+            return Initial();
+          },
+        ),
+      ),
     );
   }
 }
->>>>>>> 99382c01098e9ac8b07431ab3cfb0f6ab1cb01a4
